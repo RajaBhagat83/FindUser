f2fb0ea7443bf08a1ad73458dfea2ac4a5844716
Raja Bhagat <bhagatraja116@gmail.com>
2026-07-24 16:36:10 +0530

    import react

--- client/src/modules/Elements/Post.js
+++ client/src/modules/Elements/Post.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { BACKEND_URL } from "../../Components/config";
 import { useRecoilState } from "recoil";
 import { us, userpost } from "../../store/atoms/atom";
